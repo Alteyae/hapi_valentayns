07e4c7ab1d15da8129d1421260cb44ed4e86f8e3
Teya <altheae.a.owe@gmail.com>
2024-02-13 23:04:30 +0800

    kyot

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-       document.body.classList.add('transition-effect');
-  setTimeout(function () {
-    window.location.href = "bb.html";
-  }, 10000); 
+    // Check if animation has already been played
+    if (!sessionStorage.getItem('animationPlayed')) {
+        // Add transition effect to body
+        document.body.classList.add('transition-effect');
+
+        // Set a flag in sessionStorage indicating animation has been played
+        sessionStorage.setItem('animationPlayed', true);
+
+        // Redirect to "bb.html" after 10 seconds
+        setTimeout(function () {
+            window.location.href = "bb.html";
+        }, 10000); // 10 seconds delay
+    }
 });
