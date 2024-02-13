d871d8df0ed43fd7f47177165b5a5eed6978f303
Teya <altheae.a.owe@gmail.com>
2024-02-13 19:55:02 +0800

    yey

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+       document.body.classList.add('transition-effect');
+  setTimeout(function () {
+    window.location.href = "bb.html";
+  }, 10000); 
+});
